111a5d49870a03807e50c40e143940d1faaa0a85
Jeffrey Shen <jeffrey.shen@displayr.com>
2017-12-07 17:06:09 +1100

    start analyzing kaggle

--- kaggle.R
+++ kaggle.R
@@ -1,4 +1,13 @@
 library(data.table)
-setwd('kaggle')
-expedia_train <- fread('data/train.csv', header=TRUE)
-expedia_test <- fread('data/test.csv', header=TRUE)
+library(tidyverse)
+library(lubridate)
+types = c("date_time"="POSIXct")
+#expedia_train <- fread('nn-frameworks/data/train.csv', header=TRUE)
+#expedia_test <- fread('nn-frameworks/data/test.csv', header=TRUE)
+# save new dataset to make it run faster in the future
+expedia_train <- fread("nn-frameworks/data/train2.csv", colClasses = types )
+expedian_test <- fread("nn-frameworks/data/test2.csv")
+
+
+
+
